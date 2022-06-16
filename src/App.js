6cb490d76b1c9884f8660c0b4814a3727d3912fc
Arga Ghulam Ahmad <arga.ahmad@kargo.tech>
2022-06-16 13:28:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import './App.css';
 
 import Data from "./components/Data";
 import Uploader from './components/Uploader';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Uploader/>
-            <Data/>
+            <Router>
+                <div className="App">
+                    <div className="AppContent">
+                        <Switch>
+                            <Route exact path="/">
+                                <Data/>
+                            </Route>
+                            <Route exact path="/upload">
+                                <Uploader/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
         </div>
     );
 }
