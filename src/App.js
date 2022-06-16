0b24cc0392708397a649f1802618376a3fc850ba
Arga Ghulam Ahmad <arga.ahmad@kargo.tech>
2022-06-16 13:34:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 import Data from "./components/Data";
 import Uploader from './components/Uploader';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {Divider, Menu} from "antd";
 
 function App() {
     return (
@@ -10,6 +11,18 @@ function App() {
             <Router>
                 <div className="App">
                     <div className="AppContent">
+                        <Divider orientation="center">
+                            <h1>Teligobot</h1>
+                            <Menu mode="horizontal">
+                                <Menu.Item key="home">
+                                    <Link to="/">Home</Link>
+                                </Menu.Item>
+                                <Menu.Item key="uploadFile">
+                                    <Link to="/upload">Upload</Link>
+                                </Menu.Item>
+                            </Menu>
+                        </Divider>
+
                         <Switch>
                             <Route exact path="/">
                                 <Data/>
