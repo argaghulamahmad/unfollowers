9a347336333c21d45b4b5f2b312dd9183575cfd7
Arga Ghulam Ahmad <arga.ahmad@kargo.tech>
2022-06-16 11:19:13 +0700

    update

---
+++ src/components/Uploader.js
@@ -0,0 +1,72 @@
+import {InboxOutlined} from '@ant-design/icons';
+import {Upload} from 'antd';
+
+const {Dragger} = Upload;
+
+const App = () => (
+    <Dragger {...{
+        name: 'file',
+        multiple: true,
+
+        onDrop(e) {
+            Array.from(e.dataTransfer.files).forEach(file => {
+                console.log(file)
+
+                const reader = new FileReader();
+                reader.readAsText(file, "UTF-8");
+
+                if (file.name === "followers.json" || file.name === "following.json") {
+                    reader.onload = function (evt) {
+                        let result = evt.target.result;
+                        switch (file.name) {
+                            case "followers.json":
+                                // code block
+                                const {relationships_followers: followers} = JSON.parse(result)
+                                localStorage.setItem('followers', JSON.stringify(followers.map(item => {
+                                    return item.string_list_data[0].value
+                                })));
+                                break;
+                            case "following.json":
+                                // code block
+                                const {relationships_following: following} = JSON.parse(result)
+                                localStorage.setItem('following', JSON.stringify(following.map(item => {
+                                    return item.string_list_data[0].value
+                                })));
+                                break;
+                            default:
+                                // code block
+                                console.error()
+                        }
+                        const followers = JSON.parse(localStorage.getItem('followers'));
+                        const following = JSON.parse(localStorage.getItem('following'));
+
+                        const followback = followers.filter(username => !following.includes(username))
+                        const unfollower = following.filter(username => !followers.includes(username))
+                        const mutual = following.filter(username => followers.includes(username))
+
+                        localStorage.setItem('followback', JSON.stringify(followback));
+                        localStorage.setItem('unfollower', JSON.stringify(unfollower));
+                        localStorage.setItem('mutual', JSON.stringify(mutual));
+                    }
+                    reader.onerror = function (evt) {
+                        console.error()
+                    }
+                } else {
+                    console.error()
+                }
+            });
+        },
+    }}>
+        <p className="ant-upload-drag-icon">
+            <InboxOutlined/>
+        </p>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">
+            Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+            band files
+        </p>
+
+    </Dragger>
+);
+
+export default App;
