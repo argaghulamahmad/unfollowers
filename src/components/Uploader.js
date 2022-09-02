4f95a1cfbc6c2c73774d0dc908fd398ad366c0b3
Arga Ghulam Ahmad <arga.ahmad@kargo.tech>
2022-09-02 15:24:00 +0700

    tidy up upload page

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,5 +1,5 @@
 import {InboxOutlined} from '@ant-design/icons';
-import {Upload, notification, Button} from "antd";
+import {Upload, notification, Button, Divider, Space} from "antd";
 import {acceptedUploadedFilenames, followersJsonFileName, followingJsonFileName} from "../consts";
 
 const {Dragger} = Upload;
@@ -13,138 +13,151 @@ class Profile {
 
 const Uploader = () => (
     <div>
-        <Button type="primary" style={{width: '60%', margin: '20px'}} onClick={() => {
-            window.open('https://www.instagram.com/download/request/');
-        }}>Download files here</Button>
-        <Dragger {...{
-            name: 'file',
-            multiple: true,
+        <div>
+            <Space direction="vertical" size="middle" style={{display: 'flex'}}>
+                <div>
+                    Upload your followers and following data to get started!
+                </div>
 
-            onDrop(e) {
-                Array.from(e.dataTransfer.files).forEach(file => {
-                    const reader = new FileReader();
-                    reader.readAsText(file, "UTF-8");
-                    if (acceptedUploadedFilenames.includes(file.name)) {
-                        reader.onload = evt => {
-                            let result = evt.target.result;
+                <Button type="primary" style={{width: '100%'}} onClick={() => {
+                    window.open('https://www.instagram.com/download/request/');
+                }}>Ask for backup data here</Button>
 
-                            const storedAllProfiles = JSON.parse(localStorage.getItem('allProfiles')) || [];
-                            let allProfiles = []
-                            allProfiles = allProfiles.concat(storedAllProfiles)
+                <Button type="secondary" style={{width: '100%'}} onClick={() => {
+                    window.open('https://mail.google.com/mail/u/0/#inbox');
+                }}>Download backup data from email that instagram sent</Button>
 
-                            switch (file.name) {
-                                case followersJsonFileName:
-                                    const {relationships_followers: followersJsonParsedResult} = JSON.parse(result)
+                <Dragger {...{
+                    name: 'file',
+                    multiple: true,
 
-                                    let followerProfiles = followersJsonParsedResult.map(item => {
-                                        let data = item.string_list_data[0];
-                                        let {value: username} = data;
-                                        let {timestamp: connectedAt} = data;
-                                        return new Profile(username, connectedAt)
+                    onDrop(e) {
+                        Array.from(e.dataTransfer.files).forEach(file => {
+                            const reader = new FileReader();
+                            reader.readAsText(file, "UTF-8");
+                            if (acceptedUploadedFilenames.includes(file.name)) {
+                                reader.onload = evt => {
+                                    let result = evt.target.result;
+
+                                    const storedAllProfiles = JSON.parse(localStorage.getItem('allProfiles')) || [];
+                                    let allProfiles = []
+                                    allProfiles = allProfiles.concat(storedAllProfiles)
+
+                                    switch (file.name) {
+                                        case followersJsonFileName:
+                                            const {relationships_followers: followersJsonParsedResult} = JSON.parse(result)
+
+                                            let followerProfiles = followersJsonParsedResult.map(item => {
+                                                let data = item.string_list_data[0];
+                                                let {value: username} = data;
+                                                let {timestamp: connectedAt} = data;
+                                                return new Profile(username, connectedAt)
+                                            });
+                                            localStorage.setItem('followerProfiles', JSON.stringify(followerProfiles));
+
+                                            allProfiles = allProfiles.concat(followerProfiles);
+
+                                            localStorage.setItem('followerUsernames', JSON.stringify(followersJsonParsedResult.map(item => {
+                                                return item.string_list_data[0].value
+                                            })));
+
+                                            notification.success({
+                                                message: 'File followers.json valid',
+                                                description: 'Followers record updated!',
+                                            });
+                                            break;
+                                        case followingJsonFileName:
+                                            const {relationships_following: followingJsonParsedResult} = JSON.parse(result)
+
+                                            let followingProfiles = followingJsonParsedResult.map(item => {
+                                                let data = item.string_list_data[0];
+                                                let {value: username} = data;
+                                                let {timestamp: connectedAt} = data;
+                                                return new Profile(username, connectedAt)
+                                            });
+                                            localStorage.setItem('followingProfiles', JSON.stringify(followingProfiles));
+
+                                            allProfiles = allProfiles.concat(followingProfiles);
+
+                                            localStorage.setItem('followingUsernames', JSON.stringify(followingJsonParsedResult.map(item => {
+                                                return item.string_list_data[0].value
+                                            })));
+
+                                            notification.success({
+                                                message: 'File following.json valid',
+                                                description: 'Following record updated!',
+                                            });
+                                            break;
+                                        default:
+                                            notification.error({
+                                                message: 'Not valid json file',
+                                            });
+                                    }
+
+                                    const allProfilesMap = allProfiles.reduce((map, profile) => {
+                                        map.set(profile.username, profile);
+                                        return map;
+                                    }, new Map());
+
+                                    const allProfilesArray = Array.from(allProfilesMap.values());
+                                    allProfilesArray.sort((a, b) => {
+                                        return a.connectedAt - b.connectedAt;
                                     });
-                                    localStorage.setItem('followerProfiles', JSON.stringify(followerProfiles));
 
-                                    allProfiles = allProfiles.concat(followerProfiles);
+                                    localStorage.setItem('allProfiles', JSON.stringify(allProfilesArray));
 
-                                    localStorage.setItem('followerUsernames', JSON.stringify(followersJsonParsedResult.map(item => {
-                                        return item.string_list_data[0].value
-                                    })));
+                                    const followerUsernames = JSON.parse(localStorage.getItem('followerUsernames')) || [];
+                                    const followingUsernames = JSON.parse(localStorage.getItem('followingUsernames')) || [];
 
-                                    notification.success({
-                                        message: 'File followers.json valid',
-                                        description: 'Followers record updated!',
-                                    });
-                                    break;
-                                case followingJsonFileName:
-                                    const {relationships_following: followingJsonParsedResult} = JSON.parse(result)
+                                    const followbackUsernames = followerUsernames.filter(username => !followingUsernames.includes(username))
+                                    localStorage.setItem('followbackUsernames', JSON.stringify(followbackUsernames));
 
-                                    let followingProfiles = followingJsonParsedResult.map(item => {
-                                        let data = item.string_list_data[0];
-                                        let {value: username} = data;
-                                        let {timestamp: connectedAt} = data;
-                                        return new Profile(username, connectedAt)
-                                    });
-                                    localStorage.setItem('followingProfiles', JSON.stringify(followingProfiles));
+                                    const unfollowerUsernames = followingUsernames.filter(username => !followerUsernames.includes(username))
+                                    localStorage.setItem('unfollowerUsernames', JSON.stringify(unfollowerUsernames));
 
-                                    allProfiles = allProfiles.concat(followingProfiles);
+                                    const mutualUsernames = followingUsernames.filter(username => followerUsernames.includes(username))
+                                    localStorage.setItem('mutualUsernames', JSON.stringify(mutualUsernames));
 
-                                    localStorage.setItem('followingUsernames', JSON.stringify(followingJsonParsedResult.map(item => {
-                                        return item.string_list_data[0].value
-                                    })));
+                                    const followbackProfiles = followbackUsernames.map(username => {
+                                        const profile = allProfilesMap.get(username);
+                                        return new Profile(username, profile.connectedAt)
+                                    })
+                                    localStorage.setItem('followbackProfiles', JSON.stringify(followbackProfiles));
 
-                                    notification.success({
-                                        message: 'File following.json valid',
-                                        description: 'Following record updated!',
-                                    });
-                                    break;
-                                default:
+                                    const unfollowbackProfiles = unfollowerUsernames.map(username => {
+                                        const profile = allProfilesMap.get(username);
+                                        return new Profile(username, profile.connectedAt)
+                                    })
+                                    localStorage.setItem('unfollowerProfiles', JSON.stringify(unfollowbackProfiles));
+
+                                    const mutualProfiles = mutualUsernames.map(username => {
+                                        const profile = allProfilesMap.get(username);
+                                        return new Profile(username, profile.connectedAt)
+                                    })
+                                    localStorage.setItem('mutualProfiles', JSON.stringify(mutualProfiles));
+
+                                    localStorage.setItem('lastUpdateAt', (new Date()).toDateString())
+                                }
+                                reader.onerror = () => {
                                     notification.error({
                                         message: 'Not valid json file',
                                     });
+                                }
+                            } else {
+                                notification.error({
+                                    message: 'Please drop following and followers json file',
+                                });
                             }
-
-                            const allProfilesMap = allProfiles.reduce((map, profile) => {
-                                map.set(profile.username, profile);
-                                return map;
-                            }, new Map());
-
-                            const allProfilesArray = Array.from(allProfilesMap.values());
-                            allProfilesArray.sort((a, b) => {
-                                return a.connectedAt - b.connectedAt;
-                            });
-
-                            localStorage.setItem('allProfiles', JSON.stringify(allProfilesArray));
-
-                            const followerUsernames = JSON.parse(localStorage.getItem('followerUsernames')) || [];
-                            const followingUsernames = JSON.parse(localStorage.getItem('followingUsernames')) || [];
-
-                            const followbackUsernames = followerUsernames.filter(username => !followingUsernames.includes(username))
-                            localStorage.setItem('followbackUsernames', JSON.stringify(followbackUsernames));
-
-                            const unfollowerUsernames = followingUsernames.filter(username => !followerUsernames.includes(username))
-                            localStorage.setItem('unfollowerUsernames', JSON.stringify(unfollowerUsernames));
-
-                            const mutualUsernames = followingUsernames.filter(username => followerUsernames.includes(username))
-                            localStorage.setItem('mutualUsernames', JSON.stringify(mutualUsernames));
-
-                            const followbackProfiles = followbackUsernames.map(username => {
-                                const profile = allProfilesMap.get(username);
-                                return new Profile(username, profile.connectedAt)
-                            })
-                            localStorage.setItem('followbackProfiles', JSON.stringify(followbackProfiles));
-
-                            const unfollowbackProfiles = unfollowerUsernames.map(username => {
-                                const profile = allProfilesMap.get(username);
-                                return new Profile(username, profile.connectedAt)
-                            })
-                            localStorage.setItem('unfollowerProfiles', JSON.stringify(unfollowbackProfiles));
-
-                            const mutualProfiles = mutualUsernames.map(username => {
-                                const profile = allProfilesMap.get(username);
-                                return new Profile(username, profile.connectedAt)
-                            })
-                            localStorage.setItem('mutualProfiles', JSON.stringify(mutualProfiles));
-
-                            localStorage.setItem('lastUpdateAt', (new Date()).toDateString())
-                        }
-                        reader.onerror = () => {
-                            notification.error({
-                                message: 'Not valid json file',
-                            });
-                        }
-                    } else {
-                        notification.error({
-                            message: 'Please drop following and followers json file',
                         });
-                    }
-                });
-            },
-        }}>
-            <p className="ant-upload-drag-icon">
-                <InboxOutlined/>
-            </p>
-            <p className="ant-upload-text">Drop followers and following json file here.</p>
-        </Dragger>
+                    },
+                }}>
+                    <p className="ant-upload-drag-icon">
+                        <InboxOutlined/>
+                    </p>
+                    <p className="ant-upload-text">Drop followers and following json file here.</p>
+                </Dragger>
+            </Space>
+        </div>
     </div>
 );
 
