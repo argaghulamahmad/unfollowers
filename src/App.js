998fe51c256f63e93b016b7ee2a6d3e96e0dd3f5
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2023-08-13 16:24:02 +0700

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './App.css';
-
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import {Divider, Menu, Space} from "antd";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { Divider, Menu, Space } from "antd";
 import Insight from "./components/Insight";
 import Uploader from './components/Uploader';
 import Config from "./components/Config";
 import Stats from "./components/Stats";
+
+const menuItems = [
+    { key: "stats", label: "Stats", path: "/stats", component: Stats },
+    { key: "insight", label: "Insight", path: "/", component: Insight },
+    { key: "uploadFile", label: "Upload", path: "/upload", component: Uploader },
+    { key: "config", label: "Config", path: "/config", component: Config },
+];
 
 function App() {
     return (
@@ -16,40 +23,18 @@ function App() {
                         <Divider orientation="center">
                             <h1>Unfollowers</h1>
                         </Divider>
-
-                        <Space direction="vertical" size="middle" style={{display: 'flex'}}>
-                            <Menu style={{
-                                position: 'relative',
-                                display: 'flex',
-                                justifyContent: 'center'
-                            }} mode="horizontal">
-                                <Menu.Item key="stats">
-                                    <Link to="/stats">Stats</Link>
-                                </Menu.Item>
-                                <Menu.Item key="insight">
-                                    <Link to="/">Insight</Link>
-                                </Menu.Item>
-                                <Menu.Item key="uploadFile">
-                                    <Link to="/upload">Upload</Link>
-                                </Menu.Item>
-                                <Menu.Item key="config">
-                                    <Link to="/config">Config</Link>
-                                </Menu.Item>
+                        <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                            <Menu style={{ position: 'relative', display: 'flex', justifyContent: 'center' }} mode="horizontal">
+                                {menuItems.map(item => (
+                                    <Menu.Item key={item.key}>
+                                        <Link to={item.path}>{item.label}</Link>
+                                    </Menu.Item>
+                                ))}
                             </Menu>
-
                             <Switch>
-                                <Route exact path="/stats">
-                                    <Stats/>
-                                </Route>
-                                <Route exact path="/">
-                                    <Insight/>
-                                </Route>
-                                <Route exact path="/upload">
-                                    <Uploader/>
-                                </Route>
-                                <Route exact path="/config">
-                                    <Config/>
-                                </Route>
+                                {menuItems.map(item => (
+                                    <Route key={item.key} exact path={item.path} render={() => <item.component />} />
+                                ))}
                             </Switch>
                         </Space>
                     </div>
